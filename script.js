09465b9e7df594c3097ef833e995788bc84046ab
sethuram333 <sethurammohan.m@gmail.com>
2026-02-02 17:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+ let freq ={}
+ for(let i=0;i<str.length;i++){
+   freq[str[i]]=(freq[str[i]] || 0)+1
+ }
+ for(let i=0;i<str.length;i++){
+    if(freq[str[i]]===1){
+        return str[i]
+    }else{
+        return null
+    }
+ }
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
